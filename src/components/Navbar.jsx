8284498b368fa74872d0e3273739a45acae6cf3a
Saran-Avinash <saranbaskarmsp@gmail.com>
2024-11-09 12:00:56 +0530

    final 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [needHelp, setNeedHelp] = useState(false);
     const navigate = useNavigate()
     const {currentUser,
       setCurrentUser,
@@ -53,9 +54,9 @@ export default function Navbar() {
 
          </button>
 
-         <div id="collapsibleContent"  className={`absolute  flex flex-col justify-start gap-3 items-start rounded-lg shadow-lg w-1/2 h-64 top-14  bg-white overflow-hidden max-h-0 transition-all duration-75 ease-out z-50`}>
-            <div className='w-14 h-14 flex justify-center  items-center rounded-full bg-gray-200'>
-            </div>
+         <div id="collapsibleContent"  className={`absolute  flex flex-col justify-start gap-3 items-start rounded-lg shadow-lg w-1/4 h-fit top-14  bg-white overflow-hidden max-h-0 transition-all duration-75 ease-out z-50`}>
+              {/* <div className='w-14 h-14 flex justify-center  items-center rounded-full bg-gray-200'>
+              </div> */}
             {/* <div className='w-full  hover:bg-gray-200  h-auto border-b-2 px-2 py-2 transition-all ease-in-out duration-500'>
                <button className='text-lg'>My profile</button>
             </div> */}
@@ -63,12 +64,27 @@ export default function Navbar() {
                <button className='text-lg hover:bg-gray-200' onClick={handleLogOut}>Logout</button>
             </div>
             <div className='w-full  hover:bg-gray-200  h-auto px-2 py-2 transition-all ease-in-out duration-500'>
-               <button className='text-lg hover:bg-gray-200' onClick={handleLogOut}>Help</button>
+               <button className='text-lg hover:bg-gray-200' onClick={()=> setNeedHelp(prev => !prev)}>Help</button>
             </div>
             
             
          </div>
        </div>
+       {needHelp && 
+         <div className='fixed inset-0 z-40 bg-gray-800 bg-opacity-50 flex items-center justify-center'>
+         <div className='bg-white p-10 rounded flex flex-col justify-center gap-2'>
+             <h1 className='text-2xl'> Welcome To our Website!!</h1>
+             <ol>
+              <li>1.   After Successfully login, navigate to Generate Password to generate the password</li>    
+              <li>2.   Then come here to add new password to add multiple accounts to the same website</li>
+              <li>2.   Then click on refresh button to refersh the recent changes made to your account</li>
+             </ol>
+         <button className='px-3 py-3 hover:bg-white hover:text-red-800 bg-red-500 w-fit h-fit hover' onClick={()=>{
+            setNeedHelp(false) 
+         }}>Close</button>
+         </div>
+         </div>}
     </div>
+
   )
 }
